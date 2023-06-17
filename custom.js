85531e5d3ebb9df45457c9b6794987f942a23050
anitatechnical97 <anitatechnical97@gmail.com>
2023-06-17 17:10:02 +0530

    Typescript Introduction

---
+++ custom.js
@@ -0,0 +1,5 @@
+var name1 = "Anita"; //      Declaration and Initialization
+console.log(name1);
+var age; //      Declaration
+age = 25;
+console.log(age);
---
+++ custom.ts
@@ -0,0 +1,12 @@
+
+
+
+var name1:string="Anita";           //      Declaration and Initialization
+console.log(name1);
+
+
+
+
+var age:number;                     //      Declaration
+age = 25;
+console.log(age);
